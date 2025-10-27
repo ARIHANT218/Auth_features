84732925aa19dfc323f83dd096acb5ac807a609c
ARIHANT <157610621+ARIHANT218@users.noreply.github.com>
2025-10-27 13:54:28 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Home from './pages/Home';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+         <Route path="*" element={<Navigate to="/" replace />} />
+        
       </Routes>
 
   </BrowserRouter>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="auth-container">
+      <div className="auth-card">
+        <h1 className="auth-title">Welcome to Auth Features 🚀</h1>
+        <p className="auth-subtitle">
+          Secure authentication system built using the MERN stack.
+        </p>
+
+        <div className="home-buttons">
+          <button
+            onClick={() => navigate("/login")}
+            className="auth-button"
+          >
+            Login
+          </button>
+          <button
+            onClick={() => navigate("/signup")}
+            className="auth-button secondary"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
