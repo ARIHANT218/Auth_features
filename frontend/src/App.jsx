3eb745aa86c093ff01aaa506bd5dfe368cda0600
ARIHANT <157610621+ARIHANT218@users.noreply.github.com>
2025-10-27 14:09:05 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-         <Route path="*" element={<Navigate to="/" replace />} />
+        
         
       </Routes>
 
